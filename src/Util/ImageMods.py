766351098ce3648130009969ccb539924df6f658
jingenito <jingenito1530@gmail.com>
2020-04-24 23:05:14 -0400

    cleaned up the util folder

--- src/Util/ImageMods.py
+++ src/Util/ImageMods.py
@@ -1,12 +1,11 @@
-from PIL import Image
-import PIL.ImageOps    
+from PIL import Image, ImageOps 
 
 def ReverseImage(originalFileName : str, newFileName : str) :
     print('Opening image...')
     image = Image.open(originalFileName)
 
     print('Inverting image...')
-    inverted_image = PIL.ImageOps.invert(image)
+    inverted_image = ImageOps.invert(image)
     inverted_image.save(newFileName)
 
     print('Saved Image!')
