01075bb870ffa386842a97a426ec1b6710f24848
jingenito <jingenito1530@gmail.com>
2020-05-04 02:19:40 -0400

    changed some things with the cantor lawn volume plot

--- src/TestBenches/volumeCantorLawnTestBench.py
+++ src/TestBenches/volumeCantorLawnTestBench.py
@@ -8,17 +8,16 @@ sys.path.append(os.path.join(app_path,'src'))
 import Util.CantorLawnVolume as clv
 
 D = 1 + (np.log(2) / np.log(3)) 
-stop_eps = 10**-3
 start_eps = 10**-6
+stop_eps = 10**-3
 
-eCount = int(np.floor(stop_eps / start_eps))
-epsSeq = np.linspace(start_eps, stop_eps, eCount)
+epsSeq = np.linspace(start_eps, stop_eps, 1000)
 
 bSeq = []
 volSeq = []
 epsAlpha = []
 
-for e in epsSeq :
+for e in epsSeq.copy() :
     vol = clv.Volume_Epsilon(e)
     eA = e**(2-D)
 
