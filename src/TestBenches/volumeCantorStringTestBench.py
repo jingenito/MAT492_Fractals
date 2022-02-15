a56475b55e7b0d2dec1abad5676288a006ac40da
Joe <jingenito@vcssoftware.com>
2022-02-14 19:39:04 -0700

    committing a some old images I forgot to commit

--- src/TestBenches/volumeCantorStringTestBench.py
+++ src/TestBenches/volumeCantorStringTestBench.py
@@ -8,25 +8,37 @@ sys.path.append(os.path.join(app_path,'src'))
 import Util.CantorStringVolume as csv
 
 D = np.log(2) / np.log(3)
+alpha = D - 0.5
 delEps = 10**-4
 
 eCount = int(np.floor(0.01 / delEps))
 epsSeq = np.linspace(delEps, 0.01, eCount)
 
-bSeq = []
 volSeq = []
-epsAlpha = []
+eAseq= []
+eBseq = []
 
 for e in epsSeq :
     vol = csv.Volume_Epsilon(e)
-    eA = e**(1 - (D - 0.01))
+    eA = e**(1 - D)
+    eB = e**(1 - alpha)
 
     volSeq.append(vol)
-    bSeq.append(vol / eA)
-    epsAlpha.append(eA)
+    eAseq.append(vol / eA)
+    eBseq.append(vol / eB)
 
-plt.plot(epsSeq,bSeq,'r')
-plt.plot(epsSeq,volSeq,'g')
-plt.plot(epsSeq,epsAlpha,'b')
-plt.legend(['V(E) / E**(1-D)','V(E)','E**(1-D)'])
+# plt.plot(epsSeq,volSeq,'g')
+# plt.plot(epsSeq,eAseq,'r')
+# plt.legend(['V(E)','V(E) / E**(1-D)'])
+# plt.show()
+
+fig, (ax1, ax2) = plt.subplots(2)
+ax1.plot(epsSeq, volSeq, 'r')
+ax1.plot(epsSeq, eAseq, 'b')
+ax1.legend(['V(E)','V(E) / E**(1-D)'])
+ax1.set(xlabel='E', ylabel='V(E)')
+ax2.plot(epsSeq, volSeq, 'r')
+ax2.plot(epsSeq, eBseq, 'b')
+ax2.legend(['V(E)','V(E) / E**(1-a)'])
+ax2.set(xlabel='E', ylabel='V(E)')
 plt.show()
--- src/main.py
+++ src/main.py
@@ -10,29 +10,43 @@ if __name__ == "__main__":
     print("7: Cantor String Volume GIF")
     print("8: Cantor Lawn Volume GIF")
     print("9: Cantor Lawn Volume Just Voume")
+    print("10: Cantor Strings 1 - x")
+    print("11: Cantor Lawns 1 - x")
    
-    inp = input("Choose an image to create:\n")
+    inp = input("Choose an image to create: ")
     if inp.isnumeric() :
         mode = int(inp)
 
     if mode == 1 :
-        FractalImages.CreateCantorStringImage((1000,1000), (475,525), 6, "images/CantorString.png")
+        FractalImages.CreateCantorStringImage((1000, 100), (40, 60), 1, "images/CantorString_1.png")
     elif mode == 2 :
-        FractalImages.CreateCantorLawnImage((1000,1000), 6, "images/CantorLawn.png")
+        FractalImages.CreateCantorLawnImage((1000,1000), 6, "images/CantorLawn_6.png")
     elif mode == 3 :
-        FractalImages.CreateCantorStringGIF((1000,1000), (475,525), 6, "images/CantorStringGIF.gif")
+        FractalImages.CreateCantorStringGIF((1000,1000), (475,525), 6, "images/CantorStringGIF_6.gif")
     elif mode == 4 :
-        FractalImages.CreateCantorLawnGIF((1000,1000), 6, "images/CantorLawnGIF.gif")
+        FractalImages.CreateCantorLawnGIF((1000,1000), 6, "images/CantorLawnGIF_6.gif")
     elif mode == 5 :
-        FractalImages.CreatCantorStringVolumeImage((1000,1000), (475,525), 6, 0.0125, "images/CantorStringVolume.png")
+        FractalImages.CreatCantorStringVolumeImage((1000, 100), (40, 60), 4, 0.0125, "images/CantorStringVolume_4.png")
     elif mode == 6 :
-        FractalImages.CreatCantorLawnVolumeImage((1000,1000), 6, 0.0125, "images/CantorLawnVolume.png")
+        FractalImages.CreatCantorLawnVolumeImage((1000,1000), 6, 0.0125, "images/CantorLawnVolume_6.png")
     elif mode == 7 :
-        FractalImages.CreateCantorStringVolumeGIF((1000,1000), (475,525), 6, 0.0125, "images/CantorStringVolumeGIF.gif")
+        FractalImages.CreateCantorStringVolumeGIF((1000,1000), (475,525), 6, 0.0125, "images/CantorStringVolumeGIF_6.gif")
     elif mode == 8 :
-        FractalImages.CreateCantorLawnVolumeGIF((1000,1000), 6, 0.0125, "images/CantorLawnVolumeGIF.gif")
+        FractalImages.CreateCantorLawnVolumeGIF((1000,1000), 6, 0.0125, "images/CantorLawnVolumeGIF_6.gif")
     elif mode == 9 :
-        FractalImages.CreateCantorLawnVolumeJustVolume((1000,1000), 6, 0.0125, "images/CantorLawnVolumeJustVolume.png")
+        FractalImages.CreateCantorLawnVolumeJustVolume((1000,1000), 6, 0.0125, "images/CantorLawnVolumeJustVolume_6.png")
+    elif mode == 10 :
+        inp = input("Choose a stopping tier x: ")
+        if inp.isnumeric() :
+            for i in range(1, int(inp) + 1) :
+                filename = "images/CantorString_" + str(i) + ".png"
+                FractalImages.CreateCantorStringImage((1000, 100), (40, 60), i, filename)
+    elif mode == 11 :
+        inp = input("Choose a stopping tier x: ")
+        if inp.isnumeric() :
+            for i in range(1, int(inp) + 1) :
+                filename = "images/CantorLawn_" + str(i) + ".png"
+                FractalImages.CreateCantorLawnImage((1000, 1000), i, filename)
     else:
         print("Invalid input.")
 
